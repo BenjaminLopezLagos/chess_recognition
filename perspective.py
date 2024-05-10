0480b5b7399e3710dd48211bef4ea7e47066eeb0
Pedro Rojas <166954170+NomekoGenkah@users.noreply.github.com>
2024-05-10 10:23:13 -0400

    Added perspective function
    los parámetros son la imagen/video y los 4 puntos para cambiar la perspectiva. regresa una imagen/video del mismo tamaño de acuerdo con los puntos dados. debería funcionar tanto con imágenes como videos.
    los puntos se transforman a float32 con numpy dentro de la misma función, se pueden pasar como sean.
    Deje en comentarios el colocar los puntos rojos debido a que es algo más estético y no necesario para hacer el cambio de perspectiva.

---
+++ perspective.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+def perspective_change(img, points):
+    height, width = img.shape[:2]
+    points = np.float32(points)
+    new_shape = np.float32([[0,0], [width,0], [0,height], [width,height]])
+    #for i in range(0, 4):
+        #cv2.circle(img, (int(points[i][0]), int(points[i][1])), 7, (0,0,255), cv2.FILLED)
+    matrix = cv2.getPerspectiveTransform(points, new_shape)
+    output = cv2.warpPerspective(img, matrix, (width,height))
+    
+    return output
+
+
+img = cv2.imread("resized.jpg")
+points = np.float32([[63,40], [332,41], [12,275], [387,275]])
+
+cv2.imshow("funcion", perspective_change(img, points))
+cv2.waitKey(0)
+
+cv2.imshow("original", img)
+cv2.waitKey(0)
